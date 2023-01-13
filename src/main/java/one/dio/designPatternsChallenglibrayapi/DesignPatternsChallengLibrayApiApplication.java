d8d61e55d8a0ab6836b2333c40eed8cd2d18e8b8
Daniela-ddlm <danieladelmondes191@gmail.com>
2023-01-13 12:09:47 -0300

    setup inicial

---
+++ src/main/java/one/dio/designPatternsChallenglibrayapi/DesignPatternsChallengLibrayApiApplication.java
@@ -0,0 +1,13 @@
+package one.dio.designPatternsChallenglibrayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsChallengLibrayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsChallengLibrayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/dio/designPatternsChallenglibrayapi/DesignPatternsChallengLibrayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.dio.designPatternsChallenglibrayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsChallengLibrayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
